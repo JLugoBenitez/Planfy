cdfc684b9fb9a270eee8f52b1e92da4c53a1be48
JLugoBenitez <javierlugobenitez7@gmail.com>
2025-10-26 23:07:21 +0100

    Primer commit

---
+++ backend/planfy-backend/src/main/java/com/planfy/backend/PlanfyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.planfy.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanfyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanfyBackendApplication.class, args);
+	}
+
+}
---
+++ backend/planfy-backend/src/test/java/com/planfy/PlanfyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanfyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
