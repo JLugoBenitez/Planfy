59e8a935b1fe720b32488504c48bea4eec8eba34
JLugoBenitez <javierlugobenitez7@gmail.com>
2025-10-27 01:57:52 +0100

    Auth metido junto a Dtos

--- backend/planfy-backend/src/main/java/com/planfy/backend/dto/auth/RegisterRequest.java
+++ backend/planfy-backend/src/main/java/com/planfy/backend/dto/auth/RegisterRequest.java
@@ -1,9 +1,10 @@
 package com.planfy.backend.dto.auth;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+@Data
 public class RegisterRequest {
     private String nombre;
     private String email;
